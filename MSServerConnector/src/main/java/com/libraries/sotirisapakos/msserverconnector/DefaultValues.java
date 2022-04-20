8c1298436a8fda05010cf42a8037d91a6739a00a
SotirisSapak <98215854+SotirisSapak@users.noreply.github.com>
2022-04-20 18:40:30 +0300

    update v.1.0.2
    change DEFAULT_REQUEST_TIMEOUT_SECONDS from 4 seconds to 20.

--- MSServerConnector/src/main/java/com/libraries/sotirisapakos/msserverconnector/DefaultValues.java
+++ MSServerConnector/src/main/java/com/libraries/sotirisapakos/msserverconnector/DefaultValues.java
@@ -6,6 +6,6 @@ public interface DefaultValues {
      * Timeout time on server request - do not need to add this at any class constructor because
      * inner variable for request timeout has, by default, this value.
      */
-    int DEFAULT_REQUEST_TIMEOUT_SECONDS = 4;
+    int DEFAULT_REQUEST_TIMEOUT_SECONDS = 20;
 
 }
